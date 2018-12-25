6bda9497ad17f890b1af80c9cd7792aeb09dda34
sladkovm <sladkovm@gmail.com>
2018-12-25 17:47:50 +0100

    strava oauth

---
+++ api.py
@@ -0,0 +1,45 @@
+import responder
+import requests
+import os
+import urllib
+
+api = responder.API()
+
+
+def authorize_url():
+    """Generate authorization uri"""
+    params = {
+        "client_id": os.getenv('STRAVA_CLIENT_ID'),
+        "response_type": "code",
+        "redirect_uri": "http://localhost:5042/authorization_successful",
+        "scope": "read",
+        "state": 'state+',
+        "approval_prompt": "force"
+    }
+    values_url = urllib.parse.urlencode(params)
+    base_url = 'https://www.strava.com/oauth/authorize'
+    rv = base_url + '?' + values_url
+    return rv
+
+
+@api.route("/authorize")
+def authorize(req, resp):
+    """Redirect user to the Strava Authorization page"""
+    api.redirect(resp, location=authorize_url())
+
+
+@api.route("/authorization_successful")
+def authorization_successful(req, resp):
+    """Exchange code for a user token"""
+    params = {
+        "client_id": os.getenv('STRAVA_CLIENT_ID'),
+        "client_secret": os.getenv('STRAVA_CLIENT_SECRET'),
+        "code": req.params.get('code'),
+        "grant_type": "authorization_code"
+    }
+    r = requests.post("https://www.strava.com/oauth/token", params)
+    resp.text = r.text
+
+
+if __name__ == "__main__":
+    api.run(debug=True)
